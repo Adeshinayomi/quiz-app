7f3c6ed39ed2602a855cdecf92eac66c74e28286
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-04-30 03:03:07 +0100

    made the user icon interactive

--- src/homepage.js
+++ src/homepage.js
@@ -1,7 +1,13 @@
 let username=localStorage.getItem('username')
- document.querySelector('.js-user-name').innerHTML=username
+const userName=document.querySelector('.js-user-name');
+
+  userName.innerHTML=username
   const sidebar=document.querySelector('.js-side-bar');
-  
+
   document.querySelector('.js-bar-icon').addEventListener('click',()=>{
-  sidebar.classList.toggle('visible')
+   sidebar.classList.toggle('visible')
+  })
+
+ document.querySelector('.js-user').addEventListener('click',()=>{
+  userName.classList.toggle('visible')
  })
