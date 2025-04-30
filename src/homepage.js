748f1248a966740d4f9268919f6cda2ef6873ad4
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-04-30 02:39:42 +0100

    made the bar icon interactive

--- src/homepage.js
+++ src/homepage.js
@@ -1,2 +1,7 @@
 let username=localStorage.getItem('username')
  document.querySelector('.js-user-name').innerHTML=username
+  const sidebar=document.querySelector('.js-side-bar');
+  
+  document.querySelector('.js-bar-icon').addEventListener('click',()=>{
+  sidebar.classList.toggle('visible')
+ })
