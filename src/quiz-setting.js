0c2cc4a20812bd3236783850efb8f0bb4da71aeb
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-05-02 03:37:43 +0100

    finished the interactivity of the setting page

--- src/quiz-setting.js
+++ src/quiz-setting.js
@@ -1,8 +1,31 @@
- function loadTopicName(){
+import { subjects } from "../data/homedata.js"
+
+function loadTopicName(){
   const url=new URL(window.location.href)
   const name=url.searchParams.get('topic')
   
   document.querySelector('.topic-name').innerHTML=name
+
+  
+ const number=document.querySelector('.js-question-number')
+ const time=document.querySelector('.js-question-time')
+ const difficulty=document.querySelector('.js-question-difficulty')
+
+ let id=""
+
+ subjects.forEach((subject)=>{
+  const topic=subject.topics
+  topic.forEach((topic)=>{
+    if(name === topic.topic){
+      id=topic.id
+    }
+  })
+
+ })
+
+ document.querySelector('.js-start-btn').addEventListener('click', ()=>{
+    window.location=`question.html?name=${name}&id=${id}&number=${number.value}&time=${time.value}&difficulty=${difficulty.value}`
+ })
  }
 
  window.addEventListener('load',()=>{
