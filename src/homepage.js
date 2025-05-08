6e40eea92ca4298acc32366598379d5b2cc03ec9
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-05-08 17:49:32 +0100

    added a log out button and a coptyright text to the menu and adjusted the media queries for phone and tablet

--- src/homepage.js
+++ src/homepage.js
@@ -1,18 +1,13 @@
 import { subjects } from "../data/homedata.js";
 
-let username=localStorage.getItem('username')
-const userName=document.querySelector('.js-user-name');
 
-  userName.innerHTML=username
   const sidebar=document.querySelector('.js-side-bar');
 
   document.querySelector('.js-bar-icon').addEventListener('click',()=>{
    sidebar.classList.toggle('visible')
   })
 
- document.querySelector('.js-user').addEventListener('click',()=>{
-  userName.classList.toggle('visible')
- });
+
 
  const category_btn= document.querySelector('.js-categories-cont')
 category_btn.addEventListener('click',()=>{
